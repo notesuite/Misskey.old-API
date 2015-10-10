491cddb9a2e75d52eb3607cea694ce37aecf70e7
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-10-10 13:32:57 +0900

    Not use gulp-plumber and gulp-notify

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,8 +4,6 @@ import { task, src, dest, watch } from 'gulp';
 import * as ts from 'gulp-typescript';
 import * as tslint from 'gulp-tslint';
 import * as del from 'del';
-import * as plumber from 'gulp-plumber';
-const notify = require('gulp-notify');
 const babel = require('gulp-babel');
 
 const tsProject = ts.createProject('tsconfig.json', <any>{
@@ -20,7 +18,6 @@ task('build', ['build:ts']);
 
 task('build:ts', () => {
 	return tsProject.src()
-		.pipe(plumber({errorHandler: notify.onError('Build error: <%= error.message %>')}))
 		.pipe(ts(tsProject))
 		.pipe(babel({
 			modules: 'commonStrict'
@@ -30,7 +27,6 @@ task('build:ts', () => {
 
 task('lint', () => {
 	return src('./src/**/*.ts')
-		.pipe(plumber({errorHandler: notify.onError('Lint error: <%= error.message %>')}))
 		.pipe(tslint(<any>{
 			tslint: require('tslint')
 		}))
