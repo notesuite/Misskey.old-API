046160e924cddce8c23c5290ee6762b16f2501b5
syuilo <syuilotan@yahoo.co.jp>
2015-10-13 23:10:16 +0900

    wip

--- src/models/application.ts
+++ src/models/application.ts
@@ -1,12 +1,72 @@
-export interface Application {
-	id: string;
-	createdAt: Date;
-	userId: string;
-	appKey: string;
-	callbackUrl: string;
-	description: string;
-	icon: string;
-	permissions: string[];
-	isSuspended: boolean;
-	idDeleted: boolean;
-}
+import * as mongoose from 'mongoose';
+
+const config: any = require('../config');
+
+const db = mongoose.createConnection(config.mongo.uri, config.mongo.options);
+const Schema = mongoose.Schema;
+const schema: mongoose.Schema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	userId: {
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	createdAt: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+	appKey: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	callbackUrl: {
+		type: String,
+		required: false,
+		default: null
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	developerName: {
+		type: String,
+		required: false,
+		default: null
+	},
+	developerWebsite: {
+		type: String,
+		required: false,
+		default: null
+	},
+	isDeleted: {
+		type: Boolean,
+		required: false,
+		default: false
+	},
+	isSuspended: {
+		type: Boolean,
+		required: false,
+		default: false
+	},
+	icon: {
+		type: Schema.Types.ObjectId,
+		required: false,
+		default: null
+	},
+	permissions: {
+		type: [String],
+		required: false,
+		default: []
+	}
+});
+
+schema.virtual('iconUrl').get(() =>
+{
+	return config.imageServerUrl + "/" + this.icon;
+});
+
+module.exports = db.model('Application', schema);
