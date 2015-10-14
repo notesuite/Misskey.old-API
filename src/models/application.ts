4b288d379fde175b1ed577464d8d45237550f0b6
syuilo <syuilotan@yahoo.co.jp>
2015-10-14 11:48:09 +0900

    wip

--- src/models/application.ts
+++ src/models/application.ts
@@ -4,64 +4,18 @@ const config: any = require('../config');
 
 const db = mongoose.createConnection(config.mongo.uri, config.mongo.options);
 const Schema = mongoose.Schema;
+
 const schema: mongoose.Schema = new Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	userId: {
-		type: Schema.Types.ObjectId,
-		required: true
-	},
-	createdAt: {
-		type: Date,
-		required: true,
-		default: Date.now
-	},
-	appKey: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	callbackUrl: {
-		type: String,
-		required: false,
-		default: null
-	},
-	description: {
-		type: String,
-		required: true
-	},
-	developerName: {
-		type: String,
-		required: false,
-		default: null
-	},
-	developerWebsite: {
-		type: String,
-		required: false,
-		default: null
-	},
-	isDeleted: {
-		type: Boolean,
-		required: false,
-		default: false
-	},
-	isSuspended: {
-		type: Boolean,
-		required: false,
-		default: false
-	},
-	icon: {
-		type: Schema.Types.ObjectId,
-		required: false,
-		default: null
-	},
-	permissions: {
-		type: [String],
-		required: false,
-		default: []
-	}
+	name: { type: String, required: true },
+	userId: { type: Schema.Types.ObjectId, required: true },
+	createdAt: { type: Date, required: true, default: Date.now },
+	appKey: { type: String, required: true, unique: true },
+	callbackUrl: { type: String, required: false, default: null },
+	description: { type: String, required: true },
+	isDeleted: { type: Boolean, required: false, default: false },
+	isSuspended: { type: Boolean, required: false, default: false },
+	icon: { type: Schema.Types.ObjectId, required: false, default: null },
+	permissions: { type: [String], required: false, default: [] }
 });
 
 schema.virtual('iconUrl').get(() =>
--- src/models/status.ts
+++ src/models/status.ts
@@ -1,10 +1,19 @@
-export interface Status {
-	id: string;
-	createdAt: Date;
-	userId: string;
-	appId: string;
-	cursor: number;
-	text: string;
-	images: string[];
-	isDeleted: boolean;
-}
+import * as mongoose from 'mongoose';
+
+const config: any = require('../config');
+
+const db = mongoose.createConnection(config.mongo.uri, config.mongo.options);
+const Schema = mongoose.Schema;
+
+const schema: mongoose.Schema = new Schema({
+	appId: { type: Schema.Types.ObjectId, required: false, default: null },
+	createdAt: { type: Date, required: true, default: Date.now },
+	cursor: { type: Number },
+	attacheds: { type: [Schema.Types.ObjectId], default: [] },
+	inReplyToStatusId: { type: Schema.Types.ObjectId, default: null },
+	isDeleted: { type: Boolean, required: false, default: false },
+	text: { type: String, default: null },
+	userId: { type: Schema.Types.ObjectId, required: true }
+});
+
+module.exports = db.model('Status', schema);
