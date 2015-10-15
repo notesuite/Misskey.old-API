441c8c17b3b1b34dfd3f184e7f58effb8d28978e
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-10-15 13:43:35 +0900

    M -> mongoose

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,7 @@
-import * as M from 'mongoose';
+import * as mongoose from 'mongoose';
 import { Connection } from 'mongoose';
 const mongooseAutoIncrement: any = require('mongoose-auto-increment');
 const config: any = require('../config');
 
-const db: Connection = M.createConnection(config.mongo.uri, config.mongo.options);
+const db: Connection = mongoose.createConnection(config.mongo.uri, config.mongo.options);
 mongooseAutoIncrement.initialize(db);
