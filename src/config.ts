317e2053fa10340181c4b78c2b17211968a3bff7
古谷向日葵 <Syuilotan@yahoo.co.jp>
2015-10-17 01:57:22 +0900

    Use ===

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export default require(`${process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']}/.misskey/api.json`);
+export default require(`${process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME']}/.misskey/api.json`);
