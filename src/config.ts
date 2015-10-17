e07b549e257eb271095dbe13f7812fe887fe2d80
syuilo <syuilotan@yahoo.co.jp>
2015-10-18 01:19:28 +0900

    Add export default

--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,10 @@
-/**
- * Path of home directory
- */
 export const homeDirPath: string = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-
-/**
- * Name of config directory
- */
 export const configDirName: string = '.misskey';
-
-/**
- * Name of config file
- */
 export const configFileName: string = 'api.json';
-
-/**
- * Full path of config directory
- */
 export const configDirectoryPath: string = `${homeDirPath}/${configDirName}`;
-
-/**
- * Full path of config file
- */
 export const configPath: string = `${configDirectoryPath}/${configFileName}`;
 
-export function load(): IConfig {
-	'use strict';
-	return <IConfig>require(`${homeDirPath}/.misskey/api.json`);
-}
+export default <IConfig>require(configPath);
 
 export interface IConfig {
 	env: string;
