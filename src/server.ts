15aa636bb9c48001c16eef13905b1b1de3f997f5
syuilo <syuilotan@yahoo.co.jp>
2015-10-18 23:14:47 +0900

    [WIP] implement server

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
+import * as http from 'http';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as multer from 'multer';
 import config from './config';
 
-const server: express.Express = express();
-server.disable('x-powered-by');
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(multer());
+console.log('Init server');
+console.log(config.mongo.options.user);
+
+const app: express.Express = express();
+app.disable('x-powered-by');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
 
 // Not found handler
-server.use((req: express.Request, res: express.Response) => {
+app.use((req: express.Request, res: express.Response) => {
 	res.status(404);
 	res.json({
 		error: 'API not found.'
 	});
+});
+
+const server: http.Server = app.listen(config.port.http, () => {
+	const host: string = server.address().address;
+	const port: number = server.address().port;
+
+	console.log(`Server listning at ${host}:${port}`);
 });
