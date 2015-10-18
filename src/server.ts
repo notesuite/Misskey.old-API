6b72d0435ae702e8e7094364d77e45f81d118ae9
syuilo <syuilotan@yahoo.co.jp>
2015-10-18 23:06:53 +0900

    [WIP] implement server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as multer from 'multer';
 import config from './config';
+
+var server: express.Express = express();
+server.disable('x-powered-by');
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(multer());
+
+// Not found handler
+server.use((req: express.Request, res: express.Response) => {
+	res.status(404);
+	res.json({
+		error: 'API not found.'
+	});
+});
