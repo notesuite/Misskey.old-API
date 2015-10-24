74f4583fdbb7af671eb667a16972e78273e15657
syuilo <syuilotan@yahoo.co.jp>
2015-10-24 23:49:40 +0900

    Fix bug

--- src/restHandlers/statuses/timeline.ts
+++ src/restHandlers/statuses/timeline.ts
@@ -1,14 +1,11 @@
 import { MisskeyExpressRequest } from '../../misskeyExpressRequest';
 import { MisskeyExpressResponse } from '../../misskeyExpressResponse';
 import getTimeline from '../../endpoints/statuses/timeline';
-import {IStatus} from '../../models/status';
 
 module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
 	'use strict';
-	getTimeline(req.misskeyUserId, req.body['limit'], req.body['since-cursor'], req.body['max-cursor']).then((timeline: IStatus[]) => {
-		res.apiRender(timeline.map((status: IStatus) => {
-			return status.toObject();
-		}));
+	getTimeline(req.misskeyUserId, req.body['limit'], req.body['since-cursor'], req.body['max-cursor']).then((timeline: Object[]) => {
+		res.apiRender(timeline);
 	}, (err: any) => {
 		res.apiError(500, err);
 	});
