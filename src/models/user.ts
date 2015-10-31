c35d91c1854152b1ba53847c3724fc9abea9b790
syuilo <syuilotan@yahoo.co.jp>
2015-11-01 03:33:10 +0900

    wip

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,4 @@
 import * as mongoose from 'mongoose';
-import {IPost} from './post';
-import {IAlbumFile} from './albumFile';
 import config from '../config';
 
 const Schema: typeof mongoose.Schema = mongoose.Schema;
@@ -67,34 +65,4 @@ export default (db: mongoose.Connection) => {
 	};
 
 	return db.model('User', schema, 'Users');
-}
-
-export interface IUser extends mongoose.Document {
-	name: string;
-	createdAt: Date;
-	screenName: string;
-	screenNameLower: string;
-	comment: string;
-	color: string;
-	description: string;
-	location: string;
-	websiteUrl: string;
-	lang: string;
-	email: string;
-	encryptedPassword: string;
-	credit: Number;
-	pinnedPost: mongoose.Types.ObjectId | IPost;
-	birthday: Date;
-	icon: mongoose.Types.ObjectId | IAlbumFile;
-	banner: mongoose.Types.ObjectId | IAlbumFile;
-	wallpaper: mongoose.Types.ObjectId | IAlbumFile;
-	iconPath: string;
-	bannerPath: string;
-	wallpaperPath: string;
-	isVerfied: Boolean;
-	isEmailVerified: Boolean;
-	isPro: Boolean;
-	isPrivate: Boolean;
-	isDeleted: Boolean;
-	isSuspended: Boolean;
 }
