b81681850ab651c48ada9bef7957f7c47108a5c9
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 03:31:17 +0900

    Implement album/file/rename

--- src/endpoints/album/files/rename.ts
+++ src/endpoints/album/files/rename.ts
@@ -1,11 +1,13 @@
+import * as request from 'request';
 import {AlbumFile, AlbumFolder} from '../../../models';
 import {IUser, IAlbumFile, IAlbumFolder} from '../../../interfaces';
+import config from '../../../config';
 
 export default function(user: IUser, fileId: string, name: string)
-		: Promise<Object[]> {
+		: Promise<Object> {
 	'use strict';
 
-	return new Promise((resolve: (renamedFile: Object) => void, reject: (err: any) => void) => {
+	return new Promise<Object>((resolve, reject) => {
 		AlbumFile.findById(fileId, (findErr: any, file: IAlbumFile) => {
 			if (findErr !== null) {
 				return reject(findErr);
@@ -16,7 +18,26 @@ export default function(user: IUser, fileId: string, name: string)
 			if (<string>file.user.toString() !== user.id) {
 				return reject('file-not-found');
 			}
-			file.name = name;
+			request.post({
+				url: `http://${config.userContentsServer.ip}:${config.userContentsServer.port}/rename`,
+				formData: {
+					passkey: config.userContentsServer.passkey,
+					'old-path': file.serverPath,
+					'new-name': name
+				}
+			}, (err: any, _: any, path: any) => {
+				if (err !== null) {
+					return reject(err);
+				}
+				file.name = name;
+				file.serverPath = path;
+				file.save((saveErr: any, saved: IAlbumFile) => {
+					if (saveErr !== null) {
+						return reject(saveErr);
+					}
+					resolve(saved.toObject());
+				});
+			});
 		});
 	});
 }
