600c391bcb91e0d08493857c84ac7581f433a2ac
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-05 03:08:57 +0900

    Fix bug

--- src/endpoints/album/folders/create.ts
+++ src/endpoints/album/folders/create.ts
@@ -4,6 +4,6 @@ import {IUser, IAlbumFile, IAlbumFolder} from '../../interfaces';
 export default function(user: IUser, name: string = null): Promise<Object> {
 	'use strict';
 
-	return new Promise<Object[]>(resolve, reject) => {
+	return new Promise<Object>((resolve, reject) => {
 	});
 }
