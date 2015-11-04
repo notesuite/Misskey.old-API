76d3b2fa8d1461223f5a73907dab9ea2f9dcb21f
Petitsurume <gamemannew2013@yahoo.co.jp>
2015-11-04 20:18:11 +0900

    add users/follow ?

---
+++ src/endpoints/users/follow.ts
@@ -0,0 +1,46 @@
+import {UserFollowing,User} from '../../models';
+import {IUserFollowing,IUser} from '../../interfaces';
+/**
+ * ユーザーをフォローします
+ * @followeeId: フォローされるユーザーID
+ * @followerId: フォローするユーザーID
+ */
+export default function(followeeId: string,followerId: string) :Promise<IUserFollowing> {
+	'use strict';
+	return new Promise((resolve: () => void, reject: (err:any) => void) => {
+		if (followeeId !== undefined && followeeId !== null) {
+			if (followeeId === followerId) {
+				reject("followee-is-you");
+			} else {
+				User.findById(followeeId, (err: any, user: IUser) => {
+					if (err !== null) {
+						reject(err);
+					} else if (user === null){
+						reject("followee-not-found");
+					} else {
+						UserFollowing.findOne({followee:followeeId,follower:followerId}, (err: any, UserFollowing: IUserFollowing) => {
+							if (err !== null) {
+								reject(err);
+							} else if(UserFollowing !== null) {
+								reject("already-following");
+							} else {
+								UserFollowing.create({
+									followeeId:followeeId,
+									followerId:followerId
+								}, (err: any, createdUserFollowing) => {
+									if (err) {
+										reject(err);
+									} else {
+										resolve();
+									}
+								});
+							}
+						});
+					}
+				});
+			}
+		} else {
+			reject("empty-folowee-id");
+		}
+	});
+}
---
+++ src/restHandlers/users/follow.ts
@@ -0,0 +1,12 @@
+import { MisskeyExpressRequest } from '../../misskeyExpressRequest';
+import { MisskeyExpressResponse } from '../../misskeyExpressResponse';
+import follow from '../../endpoints/users/follow';
+
+module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
+	'use strict';
+	follow(req.query['user-id'], req.misskeyUser.id).then(() => {
+		res.apiRender({kyoppie:'yuppie'});
+	}, (err: any) => {
+		res.apiError(500, err);
+	});
+};
