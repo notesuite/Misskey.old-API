1b065ef8a03d624d8e099ad5b8b3b3bc4c83eaba
Marihachi <marihachi0620@gmail.com>
2015-11-04 13:44:04 +0900

    Implement users/followers, users/followings

---
+++ src/endpoints/users/followings.ts
@@ -0,0 +1,38 @@
+import {User, UserFollowing} from '../../models';
+import {IUser, IUserFollowing} from '../../interfaces';
+
+/**
+ * 対象ユーザーがフォローしているユーザーの一覧を取得します。
+ * @user: 対象ユーザー
+ * @limit: 取得するユーザーの最大数
+ * @sinceCursor: 取得するユーザーを、設定されたカーソルよりも大きなカーソルを持つもののみに制限します
+ * @maxCursor: 取得するユーザーを、設定されたカーソルよりも小さなカーソルを持つもののみに制限します
+ */
+export default function(user: IUser, limit: number = 30, sinceCursor: number = null, maxCursor: number = null): Promise<Object[]> {
+	'use strict';
+	return new Promise((resolve: (user: Object[]) => void, reject: (err: any) => void) => {
+		var query: any;
+		if (sinceCursor !== null) {
+			query = { follower: user.id, cursor: { $gt: sinceCursor } };
+		} else if (maxCursor !== null) {
+			query = { follower: user.id, cursor: { $lt: maxCursor } };
+		} else {
+			query = { follower: user.id };
+		}
+		UserFollowing
+			.find(query)
+			.sort('-createdAt')
+			.limit(limit)
+			.populate('followee')
+			.exec((err: any, userFollowings: IUserFollowing[]) => {
+				if (err === null) {
+					const followees: any[] = (userFollowings.length > 0)
+						? userFollowings.map((userFollowing: IUserFollowing) => userFollowing.followee)
+						: [];
+					resolve(followees);
+				} else {
+					reject(err);
+				}
+			});
+	});
+};
