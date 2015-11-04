fbcc1fa7d666ed0eae1fa581fc0ef10364a0f723
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 06:02:39 +0900

    Implement album/file/move

--- src/endpoints/album/files/move.ts
+++ src/endpoints/album/files/move.ts
@@ -1,9 +1,48 @@
-import {AlbumFile, AlbumFolder} from '../../models';
-import {IUser, IAlbumFile, IAlbumFolder} from '../../interfaces';
+import {AlbumFile, AlbumFolder} from '../../../models';
+import {IUser, IAlbumFile, IAlbumFolder} from '../../../interfaces';
 
-export default function(user: IUser, folderId: string = null): Promise<Object[]> {
+export default function(user: IUser, fileId: string, folderId: string): Promise<Object> {
 	'use strict';
 
-	return new Promise<Object[]>((resolve, reject) => {
+	return new Promise<Object>((resolve, reject) => {
+		AlbumFile.findOne({
+			_id: fileId,
+			user: user.id
+		}, (findErr: any, file: IAlbumFile) => {
+			if (findErr !== null) {
+				reject(findErr);
+			} else if (file === null) {
+				reject('file-not-found');
+			} else {
+				if (folderId !== null) {
+					AlbumFolder.findOne({
+						_id: folderId,
+						user: user.id
+					}, (folderFindErr: any, folder: IAlbumFolder) => {
+						if (folderFindErr !== null) {
+							reject(folderFindErr);
+						} else if (folder === null) {
+							reject('folder-not-found');
+						} else {
+							file.folder = folder.id;
+							file.save((saveErr: any, moved: IAlbumFile) => {
+								if (saveErr !== null) {
+									return reject(saveErr);
+								}
+								resolve(moved.toObject());
+							});
+						}
+					});
+				} else {
+					file.folder = null;
+					file.save((saveErr: any, moved: IAlbumFile) => {
+						if (saveErr !== null) {
+							return reject(saveErr);
+						}
+						resolve(moved.toObject());
+					});
+				}
+			}
+		});
 	});
 }
