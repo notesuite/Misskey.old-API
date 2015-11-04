0c4bb4aed16134ff0b17cb3eb02589418b34d447
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 06:02:28 +0900

    Update interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -14,11 +14,11 @@ export interface IUser extends mongoose.Document {
 	email: string;
 	encryptedPassword: string;
 	credit: Number;
-	pinnedPost: mongoose.Types.ObjectId | IPost;
+	pinnedPost: string | mongoose.Types.ObjectId | IPost;
 	birthday: Date;
-	icon: mongoose.Types.ObjectId | IAlbumFile;
-	banner: mongoose.Types.ObjectId | IAlbumFile;
-	wallpaper: mongoose.Types.ObjectId | IAlbumFile;
+	icon: string | mongoose.Types.ObjectId | IAlbumFile;
+	banner: string | mongoose.Types.ObjectId | IAlbumFile;
+	wallpaper: string | mongoose.Types.ObjectId | IAlbumFile;
 	iconPath: string;
 	bannerPath: string;
 	wallpaperPath: string;
@@ -33,24 +33,24 @@ export interface IUser extends mongoose.Document {
 export interface IUserFollowing extends mongoose.Document {
 	createdAt: Date;
 	cursor: number;
-	followee: mongoose.Types.ObjectId;
-	follower: mongoose.Types.ObjectId;
+	followee: string | mongoose.Types.ObjectId;
+	follower: string | mongoose.Types.ObjectId;
 }
 
 export interface IApplication extends mongoose.Document {
 	createdAt: Date;
-	userId: mongoose.Types.ObjectId;
+	userId: string | mongoose.Types.ObjectId;
 	appKey: string;
 	callbackUrl: string;
 	description: string;
-	iconId: mongoose.Types.ObjectId;
+	iconId: string | mongoose.Types.ObjectId;
 	permissions: string[];
 	isSuspended: boolean;
 	idDeleted: boolean;
 }
 
 export interface IPost extends mongoose.Document {
-	app: mongoose.Types.ObjectId | IApplication;
+	app: string | mongoose.Types.ObjectId | IApplication;
 	createdAt: Date;
 	cursor: number;
 	favoritesCount: number;
@@ -58,34 +58,34 @@ export interface IPost extends mongoose.Document {
 	repliesCount: number;
 	repostsCount: number;
 	type: string;
-	user: mongoose.Types.ObjectId | IUser;
+	user: string | mongoose.Types.ObjectId | IUser;
 }
 
 export interface IStatus extends IPost {
 	text: string;
-	attachedFiles: mongoose.Types.ObjectId[] | IAlbumFile[];
-	inReplyToPost: mongoose.Types.ObjectId | IPost;
+	attachedFiles: string | mongoose.Types.ObjectId[] | IAlbumFile[];
+	inReplyToPost: string | mongoose.Types.ObjectId | IPost;
 	isContentModified: boolean;
 	isPlain: boolean;
 }
 
 export interface IRepost extends IPost {
-	post: mongoose.Types.ObjectId | IPost;
+	post: string | mongoose.Types.ObjectId | IPost;
 }
 
 export interface IPostFavorite extends mongoose.Document {
 	createdAt: Date;
 	cursor: number;
-	post: mongoose.Types.ObjectId | IPost;
-	user: mongoose.Types.ObjectId | IUser;
+	post: string | mongoose.Types.ObjectId | IPost;
+	user: string | mongoose.Types.ObjectId | IUser;
 }
 
 export interface IAlbumFile extends mongoose.Document {
-	app: mongoose.Types.ObjectId | IApplication;
+	app: string | mongoose.Types.ObjectId | IApplication;
 	createdAt: Date;
 	cursor: number;
 	dataSize: number;
-	folder: mongoose.Types.ObjectId | IAlbumFolder;
+	folder: string | mongoose.Types.ObjectId | IAlbumFolder;
 	mimeType: string;
 	hash: string;
 	isDeleted: boolean;
@@ -94,7 +94,7 @@ export interface IAlbumFile extends mongoose.Document {
 	name: string;
 	properties: Object;
 	serverPath: string;
-	user: mongoose.Types.ObjectId | IUser;
+	user: string | mongoose.Types.ObjectId | IUser;
 }
 
 export interface IAlbumFolder extends mongoose.Document {
@@ -102,28 +102,28 @@ export interface IAlbumFolder extends mongoose.Document {
 	color: string;
 	cursor: number;
 	name: string;
-	parent: mongoose.Types.ObjectId | IAlbumFolder;
-	user: mongoose.Types.ObjectId | IUser;
+	parent: string | mongoose.Types.ObjectId | IAlbumFolder;
+	user: string | mongoose.Types.ObjectId | IUser;
 }
 
 export interface IBBSTopic extends mongoose.Document {
 	createdAt: Date;
 	cursor: number;
 	title: string;
-	user: mongoose.Types.ObjectId | IUser;
+	user: string | mongoose.Types.ObjectId | IUser;
 }
 
 export interface IBBSPost extends mongoose.Document {
-	app: mongoose.Types.ObjectId | IApplication;
-	attachedFiles: mongoose.Types.ObjectId[] | IAlbumFile[];
+	app: string | mongoose.Types.ObjectId | IApplication;
+	attachedFiles: string | mongoose.Types.ObjectId[] | IAlbumFile[];
 	createdAt: Date;
 	cursor: number;
-	inReplyToPost: mongoose.Types.ObjectId | IBBSPost;
+	inReplyToPost: string | mongoose.Types.ObjectId | IBBSPost;
 	isContentModified: boolean;
 	isDeleted: boolean;
 	isPlain: boolean;
 	plusonesCount: number;
 	text: string;
-	topic: mongoose.Types.ObjectId | IBBSTopic;
-	user: mongoose.Types.ObjectId | IUser;
+	topic: string | mongoose.Types.ObjectId | IBBSTopic;
+	user: string | mongoose.Types.ObjectId | IUser;
 }
