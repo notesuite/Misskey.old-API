4e1709822bf70efac857e0ceceb30b4e6058dd1e
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-05 03:22:54 +0900

    Clean up

--- src/endpoints/screenname-available.ts
+++ src/endpoints/screenname-available.ts
@@ -3,15 +3,12 @@ import {IUser} from '../interfaces';
 
 export default function(screenName: string): Promise<boolean> {
 	'use strict';
-	return new Promise<boolean>((resolve, reject) => {
-		if (screenName) {
-			User.find({
-				screenNameLower: screenName.toLowerCase()
-			}).limit(1).exec((err: any, user: [IUser]) => {
-				resolve(user.length === 0);
-			});
-		} else {
-			reject('empty-screen-name');
-		}
-	});
+
+	if (screenName) {
+		return Promise.resolve(User.find({
+			screenNameLower: screenName.toLowerCase()
+		}).limit(1).exec()).then<boolean>(users => users.length === 0);
+	} else {
+		return <Promise<any>>Promise.reject('empty-screen-name');
+	}
 }
