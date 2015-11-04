566b8106cb52367c0389fb37d3d98c191f02eb81
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 07:30:20 +0900

    Implement album/folder/rename

--- src/endpoints/album/folders/rename.ts
+++ src/endpoints/album/folders/rename.ts
@@ -1,9 +1,31 @@
-import {AlbumFile, AlbumFolder} from '../../models';
-import {IUser, IAlbumFile, IAlbumFolder} from '../../interfaces';
+import {AlbumFile, AlbumFolder} from '../../../models';
+import {IUser, IAlbumFile, IAlbumFolder} from '../../../interfaces';
 
-export default function(user: IUser, folderId: string = null): Promise<Object[]> {
+export default function(user: IUser, folderId: string, name: string): Promise<Object> {
 	'use strict';
 
-	return new Promise<Object[]>((resolve, reject) => {
+	if (name.length > 100) {
+		return <Promise<any>>Promise.reject('too-long-name');
+	}
+
+	return new Promise<Object>((resolve, reject) => {
+		AlbumFolder.findOne({
+			_id: folderId,
+			user: user.id
+		}, (folderFindErr: any, folder: IAlbumFolder) => {
+			if (folderFindErr !== null) {
+				reject(folderFindErr);
+			} else if (folder === null) {
+				reject('folder-not-found');
+			} else {
+				folder.name = name;
+				folder.save((saveErr: any, renamed: IAlbumFolder) => {
+					if (saveErr !== null) {
+						return reject(saveErr);
+					}
+					resolve(renamed.toObject());
+				});
+			}
+		});
 	});
 }
