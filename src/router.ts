5e8296f17e1fff412c605931f2c5014028a3bf25
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 17:57:59 +0900

    Update routes

--- src/router.ts
+++ src/router.ts
@@ -8,22 +8,31 @@ interface IRoute {
 const routing: Array<IRoute> = [
 	{method: 'get', endpoint: 'login'},
 	{method: 'get', endpoint: 'screenname-available'},
+
 	{method: 'post', endpoint: 'account/create'},
 	{method: 'get', endpoint: 'account/show'},
-	{method: 'get', endpoint: 'posts/timeline'},
-	{method: 'get', endpoint: 'posts/show'},
-	{method: 'post', endpoint: 'reposts/create'},
-	{method: 'post', endpoint: 'album/files/upload'},
-	{method: 'get', endpoint: 'album/files/list'},
-	{method: 'put', endpoint: 'album/files/move'},
-	{method: 'put', endpoint: 'album/files/rename'},
-	{method: 'delete', endpoint: 'album/files/delete'},
+
 	{method: 'get', endpoint: 'users/show'},
 	{method: 'post', endpoint: 'users/follow'},
 	{method: 'delete', endpoint: 'users/unfollow'},
 	{method: 'get', endpoint: 'users/followings'},
 	{method: 'get', endpoint: 'users/followers'},
-	{method: 'post', endpoint: 'statuses/update'}
+
+	{method: 'get', endpoint: 'posts/timeline'},
+	{method: 'get', endpoint: 'posts/show'},
+
+	{method: 'post', endpoint: 'reposts/create'},
+
+	{method: 'post', endpoint: 'statuses/update'},
+
+	{method: 'post', endpoint: 'album/files/upload'},
+	{method: 'get', endpoint: 'album/files/list'},
+	{method: 'put', endpoint: 'album/files/move'},
+	{method: 'put', endpoint: 'album/files/rename'},
+	{method: 'delete', endpoint: 'album/files/delete'},
+	{method: 'post', endpoint: 'album/folders/create'},
+	{method: 'put', endpoint: 'album/folders/move'},
+	{method: 'put', endpoint: 'album/folders/rename'}
 ];
 
 export default function(app: express.Express): void {
