09dee863afc9a319706efb20d65dc05439fd5346
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 22:07:51 +0900

    commit

--- src/endpoints/posts/status.ts
+++ src/endpoints/posts/status.ts
@@ -7,26 +7,25 @@ import publishUserStream from '../../core/publishUserStream';
  * @app: API利用App
  * @user: API利用ユーザー
  * @text: 本文
- * @inReplyToStatusId: 返信先StatusのID。nullを設定すると通常のStatusになります
+ * @inReplyToPostId: 返信先投稿のID。nullを設定すると通常の投稿になります
  * @attachFileIds: 添付ファイルのIDの配列
  */
-export default function(app: IApplication, userId: string, text: string, inReplyToPostId: string = null, attachFileIds: string = null)
+export default function(app: IApplication, user: string, text: string, inReplyToPostId: string = null, attachFileIds: string = null)
 		: Promise<Object> {
 	'use strict';
 
 	const maxTextLength: number = 300;
 	text = text.trim();
 
-	return new Promise((resolve: (status: Object) => void, reject: (err: any) => void) => {
-		if (text.length > maxTextLength) {
-			reject('too-long-text');
-			return;
-		}
+	if (text.length > maxTextLength) {
+		return <Promise<any>>Promise.reject('too-long-text');
+	}
 
+	return new Promise((resolve: (status: Object) => void, reject: (err: any) => void) => {
 		Status.create({
 			type: 'status',
 			app: app !== null ? app.id : null,
-			user: userId,
+			user: user.id,
 			inReplyToPost: inReplyToPostId,
 			text
 		}, (err: any, createdStatus: IStatus) => {
@@ -34,6 +33,9 @@ export default function(app: IApplication, userId: string, text: string, inReply
 				reject(err);
 			} else {
 				resolve(createdStatus.toObject());
+
+				user.postsCount++;
+				user.save();
 
 				publishUserStream(userId, {
 					type: 'post',
--- src/restHandlers/posts/status.ts
+++ src/restHandlers/posts/status.ts
@@ -4,7 +4,7 @@ import create from '../../endpoints/posts/status';
 
 module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
 	'use strict';
-	create(req.misskeyApp, req.misskeyUser.id, req.body['text'], req.body['in-reply-to-post-id']).then((status: Object) => {
+	create(req.misskeyApp, req.misskeyUser, req.body['text'], req.body['in-reply-to-post-id']).then((status: Object) => {
 		res.apiRender(status);
 	}, (err: any) => {
 		res.apiError(500, err);
