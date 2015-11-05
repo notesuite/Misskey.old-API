dfbd1b328336b3034de0b47726da2dfbffa730e6
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 18:04:17 +0900

    commit

--- src/restHandlers/album/files/list.ts
+++ src/restHandlers/album/files/list.ts
@@ -5,7 +5,7 @@ import files from '../../../endpoints/album/files/list';
 module.exports = (req: MisskeyExpressRequest, res: MisskeyExpressResponse): void => {
 	'use strict';
 
-	files(req.misskeyUser, req.query['folder'], req.query['include-folders']).then((files: Object[]) => {
+	files(req.misskeyUser, req.query['folder-id'], req.query['include-folders']).then((files: Object[]) => {
 		res.apiRender(files);
 	}, (err: any) => {
 		res.apiError(500, err);
