180061228b9a16f8c1bf7be958ce21baeaed6db6
Marihachi <marihachi0620@gmail.com>
2015-11-05 09:16:24 +0900

    Fix #discussion_r43861660

--- src/endpoints/users/followers.ts
+++ src/endpoints/users/followers.ts
@@ -11,14 +11,15 @@ import {IUser, IUserFollowing} from '../../interfaces';
 export default function(user: IUser, limit: number = 30, sinceCursor: number = null, maxCursor: number = null): Promise<Object[]> {
 	'use strict';
 	return new Promise((resolve: (user: Object[]) => void, reject: (err: any) => void) => {
-		var query: any;
-		if (sinceCursor !== null) {
-			query = { followee: user.id, cursor: { $gt: sinceCursor } };
-		} else if (maxCursor !== null) {
-			query = { followee: user.id, cursor: { $lt: maxCursor } };
-		} else {
-			query = { followee: user.id };
-		}
+		const query: any = ((): any => {
+			if (sinceCursor !== null) {
+				return { followee: user.id, cursor: { $gt: sinceCursor } };
+			} else if (maxCursor !== null) {
+				return { followee: user.id, cursor: { $lt: maxCursor } };
+			} else {
+				return { followee: user.id };
+			}
+		})();
 		UserFollowing
 			.find(query)
 			.sort('-createdAt')
--- src/endpoints/users/followings.ts
+++ src/endpoints/users/followings.ts
@@ -11,14 +11,15 @@ import {IUser, IUserFollowing} from '../../interfaces';
 export default function(user: IUser, limit: number = 30, sinceCursor: number = null, maxCursor: number = null): Promise<Object[]> {
 	'use strict';
 	return new Promise((resolve: (user: Object[]) => void, reject: (err: any) => void) => {
-		var query: any;
-		if (sinceCursor !== null) {
-			query = { follower: user.id, cursor: { $gt: sinceCursor } };
-		} else if (maxCursor !== null) {
-			query = { follower: user.id, cursor: { $lt: maxCursor } };
-		} else {
-			query = { follower: user.id };
-		}
+		const query: any = ((): any => {
+			if (sinceCursor !== null) {
+				return { follower: user.id, cursor: { $gt: sinceCursor } };
+			} else if (maxCursor !== null) {
+				return { follower: user.id, cursor: { $lt: maxCursor } };
+			} else {
+				return { follower: user.id };
+			}
+		})();
 		UserFollowing
 			.find(query)
 			.sort('-createdAt')
