422cfbd238fe0336af1880fe764801686ccdb352
syuilo <syuilotan@yahoo.co.jp>
2015-11-08 08:42:28 +0900

    Implement models

---
+++ src/models/talkHistory.ts
@@ -0,0 +1,26 @@
+import * as mongoose from 'mongoose';
+
+/* tslint:disable:variable-name */
+const Schema = mongoose.Schema;
+
+export default function(db: mongoose.Connection): mongoose.Model<mongoose.Document> {
+	'use strict';
+
+	const schema: mongoose.Schema = new Schema({
+		updatedAt: { type: Date, required: true, default: Date.now },
+		message: { type: Schema.Types.ObjectId, required: true, ref: 'TalkMessage' },
+		otherparty: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+		user: { type: Schema.Types.ObjectId, required: true, ref: 'User' }
+	});
+
+	if (!(<any>schema).options.toObject) {
+		(<any>schema).options.toObject = {};
+	}
+	(<any>schema).options.toObject.transform = (doc: any, ret: any) => {
+		ret.id = doc.id;
+		delete ret._id;
+		delete ret.__v;
+	};
+
+	return db.model('TalkHistory', schema, 'TalkHistories');
+}
---
+++ src/models/talkMessage.ts
@@ -0,0 +1,44 @@
+import * as mongoose from 'mongoose';
+// import mongooseAutoIncrement from 'mongoose-auto-increment';
+const mongooseAutoIncrement: any = require('mongoose-auto-increment');
+// import config from '../config';
+
+/* tslint:disable:variable-name */
+const Schema = mongoose.Schema;
+
+export default function(db: mongoose.Connection): mongoose.Model<mongoose.Document> {
+	'use strict';
+
+	mongooseAutoIncrement.initialize(db);
+
+	const schema: mongoose.Schema = new Schema({
+		app: { type: Schema.Types.ObjectId, required: false, default: null, ref: 'Application' },
+		attachedFiles: [{ type: Schema.Types.ObjectId, required: false, default: null, ref: 'AlbumFile' }],
+		createdAt: { type: Date, required: true, default: Date.now },
+		cursor: { type: Number },
+		isContentModified: { type: Boolean, required: false, default: false },
+		isDeleted: { type: Boolean, required: false, default: false },
+		isPlain: { type: Boolean, required: false, default: false },
+		isRead: { type: Boolean, required: false, default: false },
+		otherparty: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+		text: { type: String, required: true },
+		user: { type: Schema.Types.ObjectId, required: true, ref: 'User' }
+	});
+
+	if (!(<any>schema).options.toObject) {
+		(<any>schema).options.toObject = {};
+	}
+	(<any>schema).options.toObject.transform = (doc: any, ret: any) => {
+		ret.id = doc.id;
+		delete ret._id;
+		delete ret.__v;
+	};
+
+	// Auto increment
+	schema.plugin(mongooseAutoIncrement.plugin, {
+		model: 'TalkMessage',
+		field: 'cursor'
+	});
+
+	return db.model('TalkMessage', schema, 'TalkMessages');
+}
