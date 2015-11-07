3733d61fdeb689f0a7510fcebd724cd85ec60a65
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-07 17:07:56 +0900

    :golf:

--- src/models.ts
+++ src/models.ts
@@ -1,15 +1,15 @@
-import * as mongoose from 'mongoose';
+import { Connection, Model, Document, createConnection } from 'mongoose';
 import config from './config';
 
-const db: mongoose.Connection = mongoose.createConnection(config.mongo.uri, config.mongo.options);
+const db: Connection = createConnection(config.mongo.uri, config.mongo.options);
 
 /* tslint:disable:variable-name */
-export const Application: mongoose.Model<mongoose.Document> = require('./models/application')(db);
-export const AlbumFile: mongoose.Model<mongoose.Document> = require('./models/albumFile')(db);
-export const AlbumFolder: mongoose.Model<mongoose.Document> = require('./models/albumFolder')(db);
-export const User: mongoose.Model<mongoose.Document> = require('./models/user')(db);
-export const UserFollowing: mongoose.Model<mongoose.Document> = require('./models/userFollowing')(db);
-export const Post: mongoose.Model<mongoose.Document> = require('./models/post').post(db);
-export const Status: mongoose.Model<mongoose.Document> = require('./models/post').status(db);
-export const Repost: mongoose.Model<mongoose.Document> = require('./models/post').repost(db);
-export const PostFavorite: mongoose.Model<mongoose.Document> = require('./models/postFavorite')(db);
+export const Application: Model<Document> = require('./models/application')(db);
+export const AlbumFile: Model<Document> = require('./models/albumFile')(db);
+export const AlbumFolder: Model<Document> = require('./models/albumFolder')(db);
+export const User: Model<Document> = require('./models/user')(db);
+export const UserFollowing: Model<Document> = require('./models/userFollowing')(db);
+export const Post: Model<Document> = require('./models/post').post(db);
+export const Status: Model<Document> = require('./models/post').status(db);
+export const Repost: Model<Document> = require('./models/post').repost(db);
+export const PostFavorite: Model<Document> = require('./models/postFavorite')(db);
