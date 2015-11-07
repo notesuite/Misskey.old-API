024175105a76e97dd0438edbf6cf2f88c40e4715
syuilo <syuilotan@yahoo.co.jp>
2015-11-08 08:42:42 +0900

    Fix bug

--- src/models.ts
+++ src/models.ts
@@ -4,12 +4,12 @@ import config from './config';
 const db: Connection = createConnection(config.mongo.uri, config.mongo.options);
 
 /* tslint:disable:variable-name */
-export const Application: Model<Document> = require('./models/application')(db);
-export const AlbumFile: Model<Document> = require('./models/albumFile')(db);
-export const AlbumFolder: Model<Document> = require('./models/albumFolder')(db);
-export const User: Model<Document> = require('./models/user')(db);
-export const UserFollowing: Model<Document> = require('./models/userFollowing')(db);
+export const Application: Model<Document> = require('./models/application').default(db);
+export const AlbumFile: Model<Document> = require('./models/albumFile').default(db);
+export const AlbumFolder: Model<Document> = require('./models/albumFolder').default(db);
+export const User: Model<Document> = require('./models/user').default(db);
+export const UserFollowing: Model<Document> = require('./models/userFollowing').default(db);
 export const Post: Model<Document> = require('./models/post').post(db);
 export const Status: Model<Document> = require('./models/post').status(db);
 export const Repost: Model<Document> = require('./models/post').repost(db);
-export const PostFavorite: Model<Document> = require('./models/postFavorite')(db);
+export const PostFavorite: Model<Document> = require('./models/postFavorite').default(db);
