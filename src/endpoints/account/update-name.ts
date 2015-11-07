664b971cc5b3f935d849d457c90fac53f668038e
Petitsurume <gamemannew2013@yahoo.co.jp>
2015-11-07 16:20:48 +0900

    fix account/update-name

--- src/endpoints/account/update-name.ts
+++ src/endpoints/account/update-name.ts
@@ -1,4 +1,3 @@
-import {User} from '../../models';
 import {IUser} from '../../interfaces';
 
 /**
--- src/restHandlers/account/update-name.ts
+++ src/restHandlers/account/update-name.ts
@@ -8,6 +8,6 @@ export default function(req: MisskeyExpressRequest, res: MisskeyExpressResponse)
 	updateName(req.misskeyUser, req.body['name']).then((user: Object) => {
 		res.apiRender(user);
 	}, (err: any) => {
-		res.apiError(500,err);
+		res.apiError(500, err);
 	});
 };
