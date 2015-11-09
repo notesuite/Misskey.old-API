f8acec8857bf377ed651199da7a19da765e7410b
syuilo <syuilotan@yahoo.co.jp>
2015-11-10 07:23:23 +0900

    wip

---
+++ src/restHandlers/posts/get-photo-statuses.ts
@@ -0,0 +1,18 @@
+import { MisskeyExpressRequest } from '../../misskeyExpressRequest';
+import { MisskeyExpressResponse } from '../../misskeyExpressResponse';
+import getPhotoStatuses from '../../endpoints/posts/get-photo-statuses';
+
+export default function(req: MisskeyExpressRequest, res: MisskeyExpressResponse): void {
+	'use strict';
+	getPhotoStatuses(
+		req.misskeyUser,
+		req.query['user-id'],
+		req.query['limit'],
+		req.query['since-cursor'],
+		req.query['max-cursor'])
+	.then((timeline: Object[]) => {
+		res.apiRender(timeline);
+	}, (err: any) => {
+		res.apiError(500, err);
+	});
+}
