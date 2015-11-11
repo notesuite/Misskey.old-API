f4627b31a351f4234dee7f834abd2560ef0d6dc7
syuilo <syuilotan@yahoo.co.jp>
2015-11-11 09:56:07 +0900

    Fix bug

--- src/restHandlers/posts/status.ts
+++ src/restHandlers/posts/status.ts
@@ -4,7 +4,7 @@ import create from '../../endpoints/posts/status';
 
 export default function(req: MisskeyExpressRequest, res: MisskeyExpressResponse): void {
 	'use strict';
-	create(req.misskeyApp, req.misskeyUser, req.body['text'], req.body['in-reply-to-post-id']).then((status: Object) => {
+	create(req.misskeyApp, req.misskeyUser, req.body['text']).then((status: Object) => {
 		res.apiRender(status);
 	}, (err: any) => {
 		res.apiError(500, err);
