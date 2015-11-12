782049e299802d41c45f1f92b2a7f53265097b1e
syuilo <syuilotan@yahoo.co.jp>
2015-11-12 10:15:07 +0900

    commikt

--- src/endpoints/album/files/upload.ts
+++ src/endpoints/album/files/upload.ts
@@ -10,12 +10,18 @@ import add from '../../../core/addFileToAlbum';
  * @file: 内容
  */
 export default function(app: IApplication, user: IUser, fileName: string, mimetype: string, file: Buffer, size: number)
-	: Promise<IAlbumFile> {
+	: Promise<Object> {
 	'use strict';
 	if (fileName.length > 100) {
 		return <Promise<any>>Promise.reject('too-long-filename');
 	} else {
-		const appId: string = app !== null ? app.id : null;
-		return add(appId, user.id, fileName, mimetype, file, size);
+		return new Promise<Object>((resolve, reject) => {
+			const appId: string = app !== null ? app.id : null;
+			add(appId, user.id, fileName, mimetype, file, size).then((file: IAlbumFile) => {
+				resolve(file.toObject());
+			}, (err: any) => {
+				reject(err);
+			});
+		});
 	}
 }
