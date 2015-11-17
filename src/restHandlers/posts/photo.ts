0a8d0639cf62970d3b4535f6d48118b10a9686e0
syuilo <syuilotan@yahoo.co.jp>
2015-11-17 15:38:27 +0900

    Fix  ug

--- src/restHandlers/posts/photo.ts
+++ src/restHandlers/posts/photo.ts
@@ -5,7 +5,11 @@ import photo from '../../endpoints/posts/photo';
 export default function(req: MisskeyExpressRequest, res: MisskeyExpressResponse): void {
 	'use strict';
 	const text: string = req.body['text'];
-	const photos: string[] = req.body['photos'];
+	const photosJson = req.body['photos'];
+	if (photosJson === undefined || photosJson === null) {
+		return res.apiError(400, 'photos-required');
+	}
+	const photos = JSON.parse(photosJson);
 	const inReplyToPostId: string = req.body['in-reply-to-post-id'];
 	photo(req.misskeyApp, req.misskeyUser, photos, text, inReplyToPostId).then((photoPost: Object) => {
 		res.apiRender(photoPost);
