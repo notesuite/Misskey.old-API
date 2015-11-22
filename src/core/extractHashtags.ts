80c0e319c214e26d78f9d6a8cd36d847b5d61d05
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-11-23 08:35:21 +0900

    :golf:

--- src/core/extractHashtags.ts
+++ src/core/extractHashtags.ts
@@ -1,12 +1,5 @@
 export default function(text: string): string[] {
 	'use strict';
-	const tags = text.match(/#\S+/g);
-
-	if (tags === null) {
-		return [];
-	}
-
-	return tags.map((tag: string) => {
-		return tag.replace('#', '');
-	});
+	const tags: string[] = text.match(/#\S+/g);
+	return (tags !== null ? tags : []).map(tag => tag.replace('#', ''));
 }
