dbeb22fceb5e5f37ba83e1ffcb4e9d4f681afbcf
syuilo <syuilotan@yahoo.co.jp>
2015-11-28 01:01:45 +0900

    Implement rate limitting

--- src/endpoints/posts/status.ts
+++ src/endpoints/posts/status.ts
@@ -1,3 +1,6 @@
+import * as redis from 'redis';
+import config from '../../config';
+const Limiter: any = require('ratelimiter');
 import {Post, StatusPost} from '../../models';
 import {IApplication, IUser, IPost, IStatusPost} from '../../interfaces';
 import publishUserStream from '../../core/publish-user-stream';
@@ -14,79 +17,99 @@ import registerHashtags from '../../core/register-hashtags';
  * @param text 本文
  * @param inReplyToPostId 返信先投稿のID
  */
-export default function status(app: IApplication, user: IUser, text: string, inReplyToPostId: string = null)
-		: Promise<Object> {
+export default function status(
+	app: IApplication,
+	user: IUser,
+	text: string,
+	inReplyToPostId: string = null
+): Promise<Object> {
 	'use strict';
 
-	const maxTextLength: number = 300;
-	text = text.trim();
-
-	if (text.length > maxTextLength) {
-		return <Promise<any>>Promise.reject('too-long-text');
-	}
+	const limiter = new Limiter({
+		id: user.id + 'posts-status',
+		duration: 1000 * 60 * 60,
+		max: 100,
+		db: redis.createClient(config.redis.port, config.redis.host)
+	});
 
 	return new Promise<Object>((resolve, reject) => {
-		if (inReplyToPostId !== null) {
-			// リプライ先に指定されている投稿が実在するかチェック
-			Post.findById(inReplyToPostId, (err: any, reply: IPost) => {
-				if (err !== null) {
-					reject(err);
-				} else if (reply === null) {
-					reject('reply-source-not-found');
-				} else if (reply.isDeleted) {
-					reject('reply-source-not-found');
-				} else if (reply.type === 'repost') {
-					reject('reply-to-repost-is-not-allowed');
-				} else {
-					create(reply);
-				}
-			});
-		} else {
-			create();
-		}
-		function create(reply: IPost = null): void {
-			const hashtags: string[] = extractHashtags(text);
-			StatusPost.create({
-				type: 'status',
-				app: app !== null ? app.id : null,
-				user: user.id,
-				inReplyToPost: inReplyToPostId,
-				text,
-				hashtags
-			}, (createErr: any, createdStatus: IStatusPost) => {
-				if (createErr !== null) {
-					reject(createErr);
-				} else {
-					populateAll(createdStatus).then((populated: Object) => {
-						serializePost(populated, user).then((serialized: Object) => {
-							resolve(serialized);
-						}, (serializeErr: any) => {
-							reject(serializeErr);
-						});
-					}, (populateErr: any) => {
-						reject(populateErr);
-					});
+		limiter.get((limitErr: any, limit: any) => {
+			if (limitErr !== null) {
+				return reject('something-happened');
+			} else if (limit.remaining === 0) {
+				return reject('rate-limit-exceeded');
+			}
 
-					user.postsCount++;
-					user.save();
+			const maxTextLength: number = 300;
+			text = text.trim();
 
-					if (reply !== null) {
-						reply.repliesCount++;
-						reply.save();
+			if (text.length > maxTextLength) {
+				return reject('too-long-text');
+			}
+
+			if (inReplyToPostId !== null) {
+				// リプライ先に指定されている投稿が実在するかチェック
+				Post.findById(inReplyToPostId, (err: any, reply: IPost) => {
+					if (err !== null) {
+						reject(err);
+					} else if (reply === null) {
+						reject('reply-source-not-found');
+					} else if (reply.isDeleted) {
+						reject('reply-source-not-found');
+					} else if (reply.type === 'repost') {
+						reject('reply-to-repost-is-not-allowed');
+					} else {
+						create(reply);
 					}
+				});
+			} else {
+				create();
+			}
 
-					registerHashtags(hashtags);
+			function create(reply: IPost = null): void {
+				const hashtags: string[] = extractHashtags(text);
+				StatusPost.create({
+					type: 'status',
+					app: app !== null ? app.id : null,
+					user: user.id,
+					inReplyToPost: inReplyToPostId,
+					text,
+					hashtags
+				}, (createErr: any, createdStatus: IStatusPost) => {
+					if (createErr !== null) {
+						reject(createErr);
+					} else {
+						populateAll(createdStatus).then((populated: Object) => {
+							serializePost(populated, user).then((serialized: Object) => {
+								resolve(serialized);
+							}, (serializeErr: any) => {
+								reject(serializeErr);
+							});
+						}, (populateErr: any) => {
+							reject(populateErr);
+						});
 
-					savePostMentions(createdStatus, text);
+						user.postsCount++;
+						user.save();
 
-					publishUserStream(user.id, {
-						type: 'post',
-						value: {
-							id: createdStatus.id
+						if (reply !== null) {
+							reply.repliesCount++;
+							reply.save();
 						}
-					});
-				}
-			});
-		}
+
+						registerHashtags(hashtags);
+
+						savePostMentions(createdStatus, text);
+
+						publishUserStream(user.id, {
+							type: 'post',
+							value: {
+								id: createdStatus.id
+							}
+						});
+					}
+				});
+			}
+		});
 	});
 }
