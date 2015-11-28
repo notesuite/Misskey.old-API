e0a4f3aa55a3539585990f71aa5dbaf2e1e4352b
syuilo <syuilotan@yahoo.co.jp>
2015-11-29 04:23:05 +0900

    Update model

--- src/models/talk-history.ts
+++ src/models/talk-history.ts
@@ -3,12 +3,16 @@ import { Schema, Connection, Document, Model } from 'mongoose';
 export default function talkHistory(db: Connection): Model<Document> {
 	'use strict';
 
+	const deepPopulate: any = require('mongoose-deep-populate')(db);
+
 	const schema = new Schema({
 		updatedAt: { type: Date, required: true, default: Date.now },
 		message: { type: Schema.Types.ObjectId, required: true, ref: 'TalkMessage' },
 		otherparty: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
 		user: { type: Schema.Types.ObjectId, required: true, ref: 'User' }
 	});
+
+	schema.plugin(deepPopulate);
 
 	if (!(<any>schema).options.toObject) {
 		(<any>schema).options.toObject = {};
