8c8afcb3e2158c56b879f22d2bb40d4518beb5b6
syuilo <syuilotan@yahoo.co.jp>
2015-12-01 13:06:26 +0900

    Implement account/update-location

---
+++ src/endpoints/account/update-location.ts
@@ -0,0 +1,26 @@
+import {IUser} from '../../interfaces';
+
+/**
+ * ユーザーのLocationを更新します
+ * @param user API利用ユーザー
+ * @param location 新しいLocation
+ */
+export default function updateLocation(user: IUser, location: string): Promise<Object> {
+	'use strict';
+
+	location = location.trim();
+
+	if (location.length > 50) {
+		return <Promise<any>>Promise.reject('too-long-location');
+	}
+
+	return new Promise<Object>((resolve, reject) => {
+		user.location = location;
+		user.save((saveErr: any, afterUser: IUser) => {
+			if (saveErr !== null) {
+				return reject(saveErr);
+			}
+			resolve(afterUser.toObject());
+		});
+	});
+}
---
+++ src/rest-handlers/account/update-location.ts
@@ -0,0 +1,21 @@
+import * as hapi from 'hapi';
+import { IApplication, IUser } from '../../interfaces';
+import updateLocation from '../../endpoints/account/update-location';
+
+export default function updateAccountLocation(
+	app: IApplication,
+	user: IUser,
+	req: hapi.Request,
+	res: hapi.IReply
+): void {
+	'use strict';
+
+	updateLocation(
+		user,
+		req.payload['location']
+	).then(saved => {
+		res(saved);
+	}, (err: any) => {
+		res({error: err}).code(500);
+	});
+};
