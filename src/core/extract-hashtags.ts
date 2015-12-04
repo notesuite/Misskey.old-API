dc7251cf137c5106fbb040da07c2b742f6cb095e
syuilo <syuilotan@yahoo.co.jp>
2015-12-04 21:13:57 +0900

    #の前に空白がないとハッシュタグとして認識しないように修正

--- src/core/extract-hashtags.ts
+++ src/core/extract-hashtags.ts
@@ -1,5 +1,5 @@
 export default function extractHashtags(text: string): string[] {
 	'use strict';
-	const tags: string[] = text.match(/#\S+/g);
-	return (tags !== null ? tags : []).map(tag => tag.replace('#', ''));
+	const tags: string[] = text.match(/(^|\s)#(\S+)/g);
+	return (tags !== null ? tags : []).map(tag => tag.replace('#', '').trim());
 }
