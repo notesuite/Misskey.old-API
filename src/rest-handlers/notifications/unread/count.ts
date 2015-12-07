758f86e8f6132bab7480cd6891303b1e4240177f
syuilo <syuilotan@yahoo.co.jp>
2015-12-07 19:08:26 +0900

    Commit

--- src/rest-handlers/notifications/unread/count.ts
+++ src/rest-handlers/notifications/unread/count.ts
@@ -1,6 +1,6 @@
 import * as hapi from 'hapi';
 import { IApplication, IUser } from '../../../interfaces';
-import notificationsUnreadsCount from '../../../endpoints/notifications/unread/count';
+import notificationsUnreadCount from '../../../endpoints/notifications/unread/count';
 
 export default function count(
 	app: IApplication,
@@ -9,7 +9,7 @@ export default function count(
 	res: hapi.IReply
 ): void {
 	'use strict';
-	notificationsUnreadsCount(
+	notificationsUnreadCount(
 		user
 	).then(count => {
 		res(count);
