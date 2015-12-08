bc3eb6f71eccd152631e2b5d7fb1511914a05368
syuilo <syuilotan@yahoo.co.jp>
2015-12-09 02:03:47 +0900

    toLowerCase

--- src/core/register-hashtags.ts
+++ src/core/register-hashtags.ts
@@ -4,7 +4,10 @@ import { IUser, IHashtag } from '../interfaces';
 export default function registerHashtags(me: IUser, hashtags: string[]): void {
 	'use strict';
 	hashtags.forEach(hashtag => {
-		Hashtag.findOne({name: hashtag}, (err: any, existHashtag: IHashtag) => {
+		hashtag = hashtag.toLowerCase();
+		Hashtag.findOne({
+			name: hashtag
+		}, (err: any, existHashtag: IHashtag) => {
 			if (existHashtag === null) {
 				Hashtag.create({
 					name: hashtag,
