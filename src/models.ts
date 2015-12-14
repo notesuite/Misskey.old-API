0859eef0bf1ddf5dc29be4338e8b45c40fe3e7d3
syuilo <syuilotan@yahoo.co.jp>
2015-12-15 06:02:03 +0900

    [WIP] Update models

--- src/models.ts
+++ src/models.ts
@@ -14,15 +14,7 @@ import notification from './models/notification';
 import { post, status, photo, repost } from './models/post';
 import postLike from './models/post-like';
 import postMention from './models/post-mention';
-import {
-	talkUserMessage,
-	talkGroupMessage,
-	talkGroupSentInvitationActivity,
-	talkGroupMemberLeftActivity,
-	talkGroupMemberJoinActivity,
-	talkRenameGroupActivity,
-	talkTransferGroupOwnershipActivity
-} from './models/talk-message';
+import * as talk from './models/talk-message';
 import talkHistory from './models/talk-history';
 import user from './models/user';
 import userFollowing from './models/user-following';
@@ -42,13 +34,13 @@ export const PhotoPost = photo(db);
 export const Repost = repost(db);
 export const PostLike = postLike(db);
 export const PostMention = postMention(db);
-export const TalkUserMessage = talkUserMessage(db);
-export const TalkGroupMessage = talkGroupMessage(db);
-export const TalkGroupSendInvitationActivity = talkGroupSendInvitationActivity(db);
-export const TalkGroupMemberLeftActivity = talkGroupMemberLeftActivity(db);
-export const TalkGroupMemberJoinActivity = talkGroupMemberJoinActivity(db);
-export const TalkRenameGroupActivity = talkRenameGroupActivity(db);
-export const TalkTransferGroupOwnershipActivity = talkTransferGroupOwnershipActivity(db);
+export const TalkUserMessage = talk.userMessage(db);
+export const TalkGroupMessage = talk.groupMessage(db);
+export const TalkGroupSendInvitationActivity = talk.groupSentInvitationActivity(db);
+export const TalkGroupMemberLeftActivity = talk.groupMemberLeftActivity(db);
+export const TalkGroupMemberJoinActivity = talk.groupMemberJoinActivity(db);
+export const TalkRenameGroupActivity = talk.renameGroupActivity(db);
+export const TalkTransferGroupOwnershipActivity = talk.transferGroupOwnershipActivity(db);
 export const TalkHistory = talkHistory(db);
 export const User = user(db);
 export const UserFollowing = userFollowing(db);
