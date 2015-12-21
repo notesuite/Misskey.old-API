34f7087a0ef64403bf6c84244e119146eebd35a0
syuilo <syuilotan@yahoo.co.jp>
2015-12-22 08:44:42 +0900

    Commit

--- src/core/read-talk-message.ts
+++ src/core/read-talk-message.ts
@@ -1,15 +1,6 @@
-import {IUser, ITalkMessage, ITalkUserMessage, ITalkGroupMessage} from '../interfaces';
+import {TalkUserMessage, TalkGroupMessage} from '../models';
+import {IUser, ITalkMessage} from '../interfaces';
 import publishStream from './publish-streaming-message';
-
-function isUserMessage(message: ITalkMessage): message is ITalkUserMessage {
-	'use strict';
-	return message.type === 'user-message';
-}
-
-function isGroupMessage(message: ITalkMessage): message is ITalkGroupMessage {
-	'use strict';
-	return message.type === 'group-message';
-}
 
 /**
  * メッセージを既読にします
@@ -23,38 +14,44 @@ export default function(
 	'use strict';
 
 	return new Promise<void>((resolve, reject) => {
-		if (isUserMessage(message)) {
-			if ((<any>message)._doc.user.toString() === me.id.toString()) {
-				return reject('is-me');
-			}
+		switch (message.type) {
+			case 'user-message':
+				if ((<any>message)._doc.user.toString() === me.id.toString()) {
+					return reject('is-me');
+				}
 
-			const otherpartyId: string = <string>(<any>message)._doc.user;
+				const otherpartyId: string = <string>(<any>message)._doc.user;
 
-			message.isRead = true;
-			message.save();
+				TalkUserMessage.findByIdAndUpdate(message.id, { $set: { isRead: true }}, (_1, _2) => {
+					// dummy
+				});
 
-			// Publish stream message
-			publishStream(`talk-user-stream:${otherpartyId}-${me.id}`, JSON.stringify({
-				type: 'read',
-				value: message.id
-			}));
-		} else if (isGroupMessage(message)) {
-			if (message.user.toString() === me.id.toString()) {
-				return reject('is-me');
-			} else if ((<string[]>message.reads).indexOf(me.id) > 0) {
-				return reject('arleady-read');
-			}
+				// Publish stream message
+				publishStream(`talk-user-stream:${otherpartyId}-${me.id}`, JSON.stringify({
+					type: 'read',
+					value: message.id
+				}));
+				break;
+			case 'group-message':
+				if ((<any>message)._doc.user.toString() === me.id.toString()) {
+					return reject('is-me');
+				} else if ((<string[]>(<any>message)._doc.reads).indexOf(me.id) > 0) {
+					return reject('arleady-read');
+				}
 
-			(<string[]>message.reads).push(me.id);
-			message.save();
+				TalkGroupMessage.findByIdAndUpdate(message.id, { $set: { reads: (<string[]>(<any>message)._doc.reads).concat(me.id) }}, (_1, _2) => {
+					// dummy
+				});
 
-			// Publish stream message
-			publishStream(`talk-group-stream:${message.group}`, JSON.stringify({
-				type: 'read',
-				value: message.id
-			}));
-		} else {
-			resolve();
+				// Publish stream message
+				publishStream(`talk-group-stream:${(<any>message)._doc.group}`, JSON.stringify({
+					type: 'read',
+					value: message.id
+				}));
+				break;
+			default:
+				resolve();
+				break;
 		}
 	});
 }
