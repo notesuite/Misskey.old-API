1f3db913bd7bd7381f63bd0aa3b0eb74733c66af
syuilo <syuilotan@yahoo.co.jp>
2015-12-21 17:26:14 +0900

    Commit

---
+++ src/rest-handlers/talks/group/create.ts
@@ -0,0 +1,21 @@
+import { IApplication, IUser } from '../../../interfaces';
+import create from '../../../endpoints/talks/group/create';
+
+export default function(
+	app: IApplication,
+	user: IUser,
+	req: any,
+	res: any
+): void {
+	'use strict';
+	const name: string = req.payload['name'];
+	create(
+		app,
+		user,
+		name
+	).then(group => {
+		res(group);
+	}, (err: any) => {
+		res({error: err}).code(500);
+	});
+}
