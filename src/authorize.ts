156bd3a11dc4aa6352fd82cbc246c53e9ff7965a
Aya Morisawa <AyaMorisawa4869@gmail.com>
2016-01-16 20:00:50 +0900

    Fix type

--- src/authorize.ts
+++ src/authorize.ts
@@ -4,7 +4,7 @@ import config from './config';
 
 export default function(req: any): Promise<any> {
 	'use strict';
-	return new Promise<Object>((resolve, reject) => {
+	return new Promise<any>((resolve, reject) => {
 		if (req.headers['passkey'] === undefined || req.headers['passkey'] === null) {
 			resolve({ app: null, user: null });
 		} else if (req.headers['passkey'] !== config.apiPasskey) {
