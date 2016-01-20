9dbee87a6fa299d7187197d85067275551743967
syuilo <Syuilotan@yahoo.co.jp>
2016-01-20 19:31:16 +0900

    Add isOfficial

--- src/authorize.ts
+++ src/authorize.ts
@@ -2,20 +2,21 @@ import {User} from './models';
 import {IUser} from './interfaces';
 import config from './config';
 
-export default function(req: any): Promise<{ app: any, user: any }> {
+export default function(req: any): Promise<{ app: any, user: any, isOfficial: boolean }> {
 	'use strict';
-	return new Promise<{ app: any, user: any }>((resolve, reject) => {
+	return new Promise<{ app: any, user: any, isOfficial: boolean }>((resolve, reject) => {
 		if (req.headers['passkey'] === undefined || req.headers['passkey'] === null) {
-			resolve({ app: undefined, user: undefined });
+			resolve({ app: null, user: null, isOfficial: false });
 		} else if (req.headers['passkey'] !== config.apiPasskey) {
 			reject();
 		} else if (req.headers['user-id'] === null) {
-			resolve({ app: null, user: null });
+			resolve({ app: null, user: null, isOfficial: true });
 		} else {
 			User.findById(req.headers['user-id'], (err: any, user: IUser) => {
 				resolve({
 					app: null,
-					user: user
+					user: user,
+					isOfficial: true
 				});
 			});
 		}
