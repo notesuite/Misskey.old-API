8b729b8d732de433e505c3ae6efdf04000f1c8e4
Aya Morisawa <AyaMorisawa4869@gmail.com>
2016-02-02 23:06:52 +0900

    Extract forkForEachCpu

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,32 @@ import * as os from 'os';
 import checkDependencies from './check-dependencies';
 import startServer from './server';
 
-if (cluster.isMaster) {
-	logInfo('Welcome to Misskey API');
-	checkDependencies();
-	times(os.cpus().length, () => {
-		cluster.fork();
-	});
-} else {
-	startServer();
-}
+forkForEachCpu({
+	master: (): void => {
+		logInfo('Welcome to Misskey API');
+		checkDependencies();
+	},
+	worker: (): void => {
+		startServer();
+	}
+});
 
 // Fork when a worker died.
 cluster.on('exit', worker => {
 	logInfo(`(cluster: ${worker.id}) Died`);
 	cluster.fork();
 });
+
+function forkForEachCpu(callbacks: { master: () => void; worker: () => void }): void {
+	if (cluster.isMaster) {
+		callbacks.master();
+		times(os.cpus().length, () => {
+			cluster.fork();
+		});
+	} else {
+		callbacks.worker();
+	}
+}
 
 function times(n: number, f: () => void): void {
 	for (let i = 1; i <= n; i++) {
