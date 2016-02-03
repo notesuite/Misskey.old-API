c48da81f85db071e81741b979ae1af783fc35639
syuilo <syuilotan@yahoo.co.jp>
2016-02-03 10:04:29 +0900

    Fix typo

--- src/core/extract-hashtags.ts
+++ src/core/extract-hashtags.ts
@@ -7,5 +7,5 @@ export default function(text: string): string[] {
 		.map(tag => tag.trim())
 		.map(tag => tag.replace('#', '')) // 先頭の#を除去
 		.filter(tag => tag !== '')
-		.filter(tag => tag.indexOf('#') === -1); // 複数の#が含まれていたら不正なので弾く;
+		.filter(tag => tag.indexOf('#') === -1); // 複数の#が含まれていたら不正なので弾く
 }
