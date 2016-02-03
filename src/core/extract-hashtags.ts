11dcd3f17751c8c2834ecc8c142b2315b3fe7a9d
syuilo <syuilotan@yahoo.co.jp>
2016-02-03 10:03:40 +0900

    Fix bug

--- src/core/extract-hashtags.ts
+++ src/core/extract-hashtags.ts
@@ -4,6 +4,8 @@ export default function(text: string): string[] {
 	}
 	const tags: string[] = text.trim().match(/(^|\s)#(\S+)/g);
 	return (tags !== null ? tags : [])
-		.filter(tag => tag.trim().replace('#', '').indexOf('#') === -1)
-		.map(tag => tag.trim().replace('#', ''));
+		.map(tag => tag.trim())
+		.map(tag => tag.replace('#', '')) // 先頭の#を除去
+		.filter(tag => tag !== '')
+		.filter(tag => tag.indexOf('#') === -1); // 複数の#が含まれていたら不正なので弾く;
 }
