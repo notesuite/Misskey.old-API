5560c34712132a4537eda41264035f73aa47138d
syuilo <syuilotan@yahoo.co.jp>
2016-02-13 06:55:47 +0900

    Improve album/files/list

--- src/endpoints/album/files/list.ts
+++ src/endpoints/album/files/list.ts
@@ -1,3 +1,4 @@
+import {Match} from 'powerful';
 import {AlbumFile} from '../../../db/db';
 import {IUser, IAlbumFile} from '../../../db/interfaces';
 
@@ -7,14 +8,45 @@ import {IUser, IAlbumFile} from '../../../db/interfaces';
  * @param folderId 対象フォルダID(nullでルート)
  * @return ファイルオブジェクトの配列
  */
-export default function(user: IUser, folderId: string = null): Promise<Object[]> {
+export default function(
+	user: IUser,
+	folderId: string = null,
+	limit: number = 20,
+	sinceCursor: number = null,
+	maxCursor: number = null
+): Promise<Object[]> {
+	limit = parseInt(<any>limit, 10);
+
+	if (limit < 1) {
+		return <Promise<any>>Promise.reject('1 kara');
+	} else if (limit > 100) {
+		return <Promise<any>>Promise.reject('100 made');
+	}
+
 	return new Promise<Object[]>((resolve, reject) => {
-		AlbumFile.find({
+
+		let sort: any = {createdAt: -1};
+		const query = Object.assign({
 			user: user.id,
 			folder: folderId,
 			isHidden: false,
 			isDeleted: false
-		}, (filesFindErr: any, files: IAlbumFile[]) => {
+		}, new Match<void, any>(null)
+			.when(() => sinceCursor !== null, () => {
+				sort = {createdAt: 1};
+				return { cursor: { $gt: sinceCursor } };
+			})
+			.when(() => maxCursor !== null, () => {
+				return { cursor: { $lt: maxCursor } };
+			})
+			.getValue({})
+		);
+
+		AlbumFile
+		.find(query)
+		.sort(sort)
+		.limit(limit)
+		.exec(query, (filesFindErr: any, files: IAlbumFile[]) => {
 			if (filesFindErr !== null) {
 				return reject(filesFindErr);
 			}
